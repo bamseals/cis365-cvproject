ab14d4f931b33e0c7f1da63175ddceb40cf7a56d
Sam Beals <samebeals@yahoo.com>
2023-11-24 17:20:41 -0500

    google eye video

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ https://pypi.org/project/opencv-python/
 Training Cascade Classifiers:
 OpenCV - version 3.4.3 - Other versions may also work
 https://sourceforge.net/projects/opencvlibrary/files/opencv-win/3.4.3/
+
+
+Documentation, articles, and tutorials used:
+https://docs.opencv.org/4.x/db/d28/tutorial_cascade_classifier.html
+https://docs.opencv.org/4.x/dc/d88/tutorial_traincascade.html
+https://medium.com/@mumbaiyachori/train-a-custom-dataset-for-object-detection-using-haar-cascade-in-windows-f1504e9641c0
+https://sublimerobots.com/2015/02/dancing-mustaches/
--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def mustacheifyVideo():
     noseCascadeFile = cwd+'\cascades\pretrained\haarcascade_mcs_nose.xml'
     faceCascade = cv2.CascadeClassifier(faceCascadeFile)
     noseCascade = cv2.CascadeClassifier(noseCascadeFile)
-    mustacheFile = cv2.imread("img\mustache2.png", -1)
+    mustacheFile = cv2.imread("img\mustache.png", -1)
     orig = mustacheFile[:,:,3]
     orig_inverted = cv2.bitwise_not(orig)
     mustache = mustacheFile[:,:,0:3]
@@ -104,6 +104,68 @@ def mustacheifyVideo():
     stream.release()
     cv2.destroyAllWindows()
         
-mustacheifyVideo()
+# mustacheifyVideo()
+
+def googleEyeVideo():
+    cwd = os.getcwd()
+    faceCascadeFile = cwd+'\cascades\pretrained\haarcascade_frontalface_default.xml'
+    eyeCascadeFile = cwd+'\cascades\pretrained\haarcascade_eye.xml'
+    faceCascade = cv2.CascadeClassifier(faceCascadeFile)
+    eyeCascade = cv2.CascadeClassifier(eyeCascadeFile)
+    google1File = cv2.imread("img\googleeye1.png", -1)
+    google2File = cv2.imread("img\googleeye2.png", -1)
+    # left eye
+    orig1 = google1File[:,:,3]
+    orig1_inverted = cv2.bitwise_not(orig1)
+    eye1 = google1File[:,:,0:3]
+    orig1Height, orig1Width = eye1.shape[:2]
+    # right eye
+    orig2 = google2File[:,:,3]
+    orig2_inverted = cv2.bitwise_not(orig2)
+    eye2 = google2File[:,:,0:3]
+    orig2Height, orig2Width = eye2.shape[:2]
+
+    # video input stream
+    stream = cv2.VideoCapture(0)
+    while True:
+        ret, img = stream.read()
+        img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(img_grey)
+        for (x, y, w, h) in faces:
+            # cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),1)
+            face_grey = img_grey[y:y+h, x:x+w]
+            face_color = img[y:y+h, x:x+w]
+            eyes = eyeCascade.detectMultiScale(face_grey)
+            eyeindex = 0
+            for (ex,ey,ew,eh) in eyes:
+                # cv2.rectangle(face_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),1)
+                eyeindex += 1
+                if eyeindex %2 == 0:
+                    eye = eye1
+                    mask = orig1
+                    mask_inv = orig1_inverted
+                else:
+                    eye = eye2
+                    mask = orig2
+                    mask_inv = orig2_inverted
+                eyeBackground = face_color[ey:ey+eh, ex:ex+ew]
+                resizeMustache = cv2.resize(eye, (ew,eh), interpolation = cv2.INTER_AREA)
+                resizeOrig = cv2.resize(mask, (ew,eh), interpolation = cv2.INTER_AREA)
+                resizeInvert = cv2.resize(mask_inv, (ew,eh), interpolation = cv2.INTER_AREA)
+                background = cv2.bitwise_and(eyeBackground,eyeBackground,mask=resizeInvert)
+                foreground = cv2.bitwise_and(resizeMustache,resizeMustache,mask=resizeOrig)
+                overlay = cv2.add(background,foreground)
+                face_color[ey:ey+eh, ex:ex+ew] = overlay
+                
+        
+        cv2.imshow('img', img)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+    
+    stream.release()
+    cv2.destroyAllWindows()
+
+googleEyeVideo()
 
 
